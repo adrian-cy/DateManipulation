1d385ef90ae0d5b11a1413307c92a11075239a93
Adrian <adrian@fundary.net>
2024-07-02 16:31:25 -0400

    Initial Commit

---
+++ DateManipulation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DateManipulation
+//
+//  Created by Ioan Adrian Hancu on 2024-07-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DateManipulation/DateManipulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateManipulationApp.swift
+//  DateManipulation
+//
+//  Created by Ioan Adrian Hancu on 2024-07-02.
+//
+
+import SwiftUI
+
+@main
+struct DateManipulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
