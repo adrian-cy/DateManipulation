7f57af8fe5f80f010aedc641e9bb4c50ddffb757
Adrian <adrian@fundary.net>
2024-07-02 16:55:58 -0400

    Manipulate the date to get future values

--- DateManipulation/ContentView.swift
+++ DateManipulation/ContentView.swift
@@ -8,17 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    let today = Date.now
+    var twoWeeks = Date.now
+    var month = Date.now
+    
+    init() {
+//        self.twoWeeks = self.today.addingTimeInterval(60*60*24*14)
+        self.twoWeeks = Calendar.current.date(byAdding: .day, value: 14, to: today) ?? Date.now
+        self.month = Calendar.current.date(byAdding: .month, value: 1, to: today) ?? Date.now
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(today.formatted(date: .long, time: .omitted))
+            Text(twoWeeks.formatted(date: .long, time: .omitted))
+            Text(month.formatted(date: .long, time: .omitted))
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
-}
+//#Preview {
+//    ContentView()
+//}
